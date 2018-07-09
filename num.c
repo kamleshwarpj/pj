11951aeff5a7ae94a91f4690e1e313dd475b5609
kamleshwarpj <41018899+kamleshwarpj@users.noreply.github.com>
2018-07-09 22:32:40 +0530

    Rename number.c to num.c

---
+++ num.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+
+#include<conio.h>
+
+void main()
+
+{
+
+int n;
+
+scanf("%d",&n);
+if(n<0)
+
+{
+
+printf("negative");
+
+}
+
+elseif(n>0)
+
+{
+
+printf("positive");
+
+}
+
+else
+
+{
+
+printf("zero");
+}
+getch();
+
+}
